9f44aba6d6d71c68441feea4715ab5c7c8a6114a
Richards-d <dienepapa90@gmail.com>
2025-03-11 14:28:43 +0000

    Project commit

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.get("*", (req,res)=>{
 })
 
 // Database connection with MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
     console.log("connexion successful");   
 })
 .catch((error)=>console.log(error));
