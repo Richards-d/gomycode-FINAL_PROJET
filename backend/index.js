7956c232089137f03046df2f4b844a6396a4296b
Richards-d <dienepapa90@gmail.com>
2025-03-17 16:44:26 +0000

    Nextjs folders modify

--- backend/index.js
+++ backend/index.js
@@ -17,10 +17,10 @@ app.use(express.json());
 app.use(cors());
 
 // Serve static files from the frontend build folder
-app.use(express.static(path.join(__dirname, "frontend", "build")));
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
-});
+// app.use(express.static(path.join(__dirname, "frontend", "build")));
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+// });
 
 // Database connection with MongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
